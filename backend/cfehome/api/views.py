717246ad2be2c708af1d085a29804a8f0e9f643d
Emmanuel <emmanuel.dacquah.gmail.com>
2022-10-25 19:17:29 +0300

    added serializer

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,5 +1,6 @@
-from django.forms.models import model_to_dict
+# from django.forms.models import model_to_dict
 from products.models import Product
+from products.serializers import ProductSerializer
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -9,8 +10,9 @@ def api_home(request, *args, **kwargs):
     """
     Django Rest Frameworks API View
     """
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    if instance:
+        # data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
     return Response(data)
