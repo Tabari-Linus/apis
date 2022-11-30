df6db594404f43e30933db42935612f5e8afc1ac
Emmanuel <emmanuel.dacquah.gmail.com>
2022-11-30 12:38:50 +0300

    list based views

--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+# from django.http import Http404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -25,3 +29,32 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 
 
 Product_Detail_view =ProductDetailAPIView.as_view()
+
+
+@api_view(['GET','POST'])
+def product_alt_view(request, pk=None,*args, **kwargs):
+    method = request.method 
+
+    if method == 'GET':
+        if pk is not None:
+            # detail view
+            obj =get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False)
+            return Response()
+            
+        # list view
+        queryset =Product.objects.all()
+        data = ProductSerializer(queryset, many=True)
+        return Response(data)
+    if method == "POST":
+        # create an item
+        serializer = ProductSerializer(data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content')
+    
+            if content is None:
+                content = title
+                serializer.save(content = content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"}, status=400)
